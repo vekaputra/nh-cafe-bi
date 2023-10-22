ad237232897153fe3042abcc1c869f955cdf946b
vekaputra-sqe <valentino.ekaputra@smma.id>
2023-10-22 12:30:03 +0700

    do not expose mysql to outside

--- pull.sh
+++ pull.sh
@@ -7,8 +7,8 @@ fi
 cd /usr/share/github
 git clone git@github.com:vekaputra/nh-cafe-bi.git
 
-cd /usr/share/github/nh-cafe-bi/backend
-docker build -t nhcafe-bo .
+# cd /usr/share/github/nh-cafe-bi/backend
+# docker build -t nhcafe-bo .
 
 if [ ! -d "/usr/share/github/nh-cafe-bi/backend/data" ]; then
     mkdir -p /usr/share/github/nh-cafe-bi/backend/data
