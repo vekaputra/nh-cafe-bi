613459c87153134e0264fcc1acafe9df1f1cbb93
vekaputra-sqe <valentino.ekaputra@smma.id>
2023-10-22 14:00:57 +0700

    fix compose

--- compose-up.sh
+++ compose-up.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-export NH_MYSQL_ROOT_PASSWORD=
-export NH_MYSQL_USER=
-export NH_MYSQL_PASSWORD=
-
-cd /usr/share/github/nh-cafe-bi/backend
-docker-compose up -d
+cd /usr/share/github/nh-cafe-bi
+docker-compose -f docker-compose.prod.yml up -d
