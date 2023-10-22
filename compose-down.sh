952b657323050fa154caa825c6ca5827ef3eff47
vekaputra-sqe <valentino.ekaputra@smma.id>
2023-10-22 14:02:00 +0700

    fix compose

---
+++ compose-down.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd /usr/share/github/nh-cafe-bi
+docker-compose -f docker-compose.prod.yml down
--- compose-up.sh
+++ compose-up.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+cd /usr/share/github/nh-cafe-bi/backend
+docker build -t nhcafe-bo .
+
 cd /usr/share/github/nh-cafe-bi
 docker-compose -f docker-compose.prod.yml up -d
