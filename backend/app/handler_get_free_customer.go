d1d73ca039ac86d01022d6e727cb718f78945fcd
vekaputra-sqe <valentino.ekaputra@smma.id>
2023-11-06 15:15:30 +0700

    fix query bug

--- backend/app/handler_get_free_customer.go
+++ backend/app/handler_get_free_customer.go
@@ -15,7 +15,7 @@ FROM customers c
     JOIN monthly_transactions mt ON c.id = mt.customer_id
     JOIN branches b ON b.id = mt.branch_id
     LEFT JOIN customer_referral_mappings crm ON c.id = crm.customer_id AND crm.assigned_at <= mt.transaction_date
-	LEFT JOIN referral_fees rf ON crm.referral_fee_id = rf.id AND mt.branch_id = rf.branch_id
+	LEFT JOIN referral_fees rf ON crm.referral_fee_id = rf.id AND mt.branch_id = rf.branch_id AND rf.assigned_at <= mt.transaction_date
 WHERE crm.id IS NULL OR rf.id IS NULL
 GROUP BY c.id, c.customer_code, c.name, b.id, b.branch_code, b.short_name
 ORDER BY c.customer_code ASC;`
